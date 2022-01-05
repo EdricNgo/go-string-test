e99ad18eaf150657ee5c7b7d1efae80d5c5c4984
Phung Ngo <phungnm1997@gmail.com>
2022-01-06 00:06:52 +0700

    [Test] testValidity

---
+++ main_test.go
@@ -0,0 +1,60 @@
+package main
+
+import "testing"
+
+// function TestTestValidity - unit test for testValidity
+// 10 testcases
+// Difficulty: Easy
+// Estimated time: 5mins
+// Used time: 9mins
+func TestTestValidity(t *testing.T) {
+	emptyStringResult := testValidity("")
+	if emptyStringResult != false {
+		t.Errorf(`testValidity(""), failed, output expect false instead of %v`, emptyStringResult)
+	}
+
+	missingDashStringResult := testValidity("23aa-49-bb-33-aa")
+	if missingDashStringResult != false {
+		t.Errorf(`testValidity("23aa-49-bb-33-aa") failed, output expect false instead of %v`, missingDashStringResult)
+	}
+
+	emptyTextStringResult := testValidity("23--48-aa-33")
+	if emptyTextStringResult != false {
+		t.Errorf(`testValidity("23--48") failed, output expect false instead of %v`, emptyTextStringResult)
+	}
+
+	emptyNumberStringResult := testValidity("23-ab--caba-56-haha")
+	if emptyNumberStringResult != false {
+		t.Errorf(`testValidity("23-ab--caba-56-haha") failed, output expect false instead of %v`, emptyNumberStringResult)
+	}
+
+	invalidTextStringResult := testValidity("23-a3b-33-caba-56-haha")
+	if invalidTextStringResult != false {
+		t.Errorf(`testValidity("23-a3b-bb-caba-56-haha") failed, output expect false instead of %v`, invalidTextStringResult)
+	}
+
+	invalidNumberStringResult := testValidity("2a3-ab-44-caba-56-haha")
+	if invalidNumberStringResult != false {
+		t.Errorf(`testValidity("23-ab-3a3-caba-56-haha") failed, output expect false instead of %v`, invalidNumberStringResult)
+	}
+
+	invalidFormatStringResult := testValidity("23-ab-cc-caba-56-haha")
+	if invalidFormatStringResult != false {
+		t.Errorf(`testValidity("23-ab-bb-caba-56-haha") failed, output expect false instead of %v`, invalidFormatStringResult)
+	}
+
+	invalidLastChracterStringResult := testValidity("23-ab-33-caba-56-haha3")
+	if invalidLastChracterStringResult != false {
+		t.Errorf(`testValidity("23-ab-cc-caba-56-haha3") failed, output expect false instead of %v`, invalidLastChracterStringResult)
+	}
+
+	invalidFirstChracterStringResult := testValidity("ab-33-caba-56-haha")
+	if invalidFirstChracterStringResult != false {
+		t.Errorf(`testValidity("23-ab-cc-caba-56-haha3") failed, output expect false instead of %v`, invalidFirstChracterStringResult)
+	}
+
+	validStringResult := testValidity("23-ab-33-caba-56-haha")
+	if validStringResult != true {
+		t.Errorf(`testValidity("23-ab-33-caba-56-haha") failed, output expect true instead of %v`, validStringResult)
+	}
+}
